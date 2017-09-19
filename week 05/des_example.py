074e6dcc2fa6e5889d82c2a9800e976cc77bab69
wangjin0818 <wangjin0818@gmail.com>
2017-09-19 16:42:40 +0800

    black hat submit!

--- week 05/des_example.py
+++ week 05/des_example.py
@@ -6,11 +6,11 @@ import pyDes
 import binascii
 
 data = "Pleaseencrypt my data"
-k = pyDes.des("DESCRYPT",pyDes.CBC, "\0\0\0\0\0\0\0\0", pad=None, padmode=pyDes.PAD_PKCS5)
+k = pyDes.des("DESCRYPT", pyDes.CBC, "\0\0\0\0\0\0\0\0", pad=None, padmode=pyDes.PAD_PKCS5)
 
 d = k.encrypt(data)
 
-print "Encrypted:%r" % binascii.hexlify(d)
-print "Decrypted:%r" % k.decrypt(d)
+print "Encrypted: %r" % binascii.hexlify(d)
+print "Decrypted: %r" % k.decrypt(d)
 
-assert k.decrypt(d) ==data
+assert k.decrypt(d) == data
